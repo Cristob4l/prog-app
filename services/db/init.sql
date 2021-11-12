0a621b62f9872ce6948a87df00d4af79359b1b8f
Claudio Diaz <cristobal.4.diaz@gmail.com>
2021-11-12 01:20:46 -0300

    fix sql statements

--- services/db/init.sql
+++ services/db/init.sql
@@ -1,121 +1,102 @@
-CREATE TABLE "public.maquina" (
-	"id_maquina" serial NOT NULL UNIQUE,
-	"nombre" VARCHAR(255) NOT NULL,
-	"descripción" VARCHAR(255),
-	"id_estado" integer,
-	CONSTRAINT "maquina_pk" PRIMARY KEY ("id_maquina")
-)
+CREATE TABLE "maquina" (
+  "id_maquina" serial UNIQUE PRIMARY KEY,
+  "nombre" VARCHAR(255) NOT NULL,
+  "descripción" VARCHAR(255),
+  "id_estado" integer
+);
 
-CREATE TABLE "public.estado_maquina" (
-	"id_estado" serial(255) NOT NULL UNIQUE,
-	"estado" VARCHAR(255) NOT NULL,
-	"descripcion" VARCHAR(255) NOT NULL,
-	CONSTRAINT "estado_maquina_pk" PRIMARY KEY ("id_estado")
-)
+CREATE TABLE "estado_maquina" (
+  "id_estado" serial UNIQUE PRIMARY KEY NOT NULL,
+  "estado" VARCHAR(255) NOT NULL,
+  "descripcion" VARCHAR(255) NOT NULL
+);
 
-CREATE TABLE "public.incidentes" (
-	"id_incidente" serial NOT NULL,
-	"id_maquina" integer NOT NULL,
-	"descripcion" VARCHAR(255) NOT NULL,
-	"fecha" DATETIME NOT NULL,
-	CONSTRAINT "incidentes_pk" PRIMARY KEY ("id_incidente")
-)
+CREATE TABLE "incidentes" (
+  "id_incidente" serial PRIMARY KEY NOT NULL,
+  "id_maquina" integer NOT NULL,
+  "descripcion" VARCHAR(255) NOT NULL,
+  "fecha" timestamp NOT NULL
+);
 
-CREATE TABLE "public.reparacion" (
-	"id_reparacion" serial NOT NULL UNIQUE,
-	"descripcion" VARCHAR(255) NOT NULL,
-	"fecha_inicio" DATETIME NOT NULL,
-	"fecha_termino" DATETIME NOT NULL,
-	"id_maquina" integer NOT NULL,
-	"id_componentes" integer NOT NULL,
-	CONSTRAINT "reparacion_pk" PRIMARY KEY ("id_reparacion")
-)
+CREATE TABLE "reparacion" (
+  "id_reparacion" serial UNIQUE PRIMARY KEY NOT NULL,
+  "descripcion" VARCHAR(255) NOT NULL,
+  "fecha_inicio" timestamp NOT NULL,
+  "fecha_termino" timestamp NOT NULL,
+  "id_maquina" integer NOT NULL,
+  "id_componentes" integer NOT NULL
+);
 
-CREATE TABLE "public.reparacion_componente" (
-	"id_reparacion_componente" serial NOT NULL,
-	"id_componente" integer NOT NULL,
-	"id_mantencion" integer NOT NULL,
-	CONSTRAINT "reparacion_componente_pk" PRIMARY KEY ("id_reparacion_componente")
-) 
-CREATE TABLE "public.componente" (
-	"id_componente" serial NOT NULL,
-	"nombre" VARCHAR(255) NOT NULL,
-	"componente_padre" integer NOT NULL,
-	"descripcion" VARCHAR(255) NOT NULL,
-	CONSTRAINT "componente_pk" PRIMARY KEY ("id_componente")
-)
+CREATE TABLE "reparacion_componente" (
+  "id_reparacion_componente" SERIAL PRIMARY KEY NOT NULL,
+  "id_componente" integer NOT NULL,
+  "id_mantencion" integer NOT NULL
+);
 
-CREATE TABLE "public.componentes_en_maquina" (
-	"id_componente_en_maquina" serial NOT NULL,
-	"id_maquina" integer NOT NULL,
-	"id_estado" integer NOT NULL,
-	CONSTRAINT "componentes_en_maquina_pk" PRIMARY KEY ("id_componente_en_maquina")
-)
+CREATE TABLE "componente" (
+  "id_componente" serial PRIMARY KEY NOT NULL,
+  "nombre" VARCHAR(255) NOT NULL,
+  "componente_padre" integer NOT NULL,
+  "descripcion" VARCHAR(255) NOT NULL
+);
 
-CREATE TABLE "public.estado_componente" (
-	"id_estado" serial(255) NOT NULL UNIQUE,
-	"estado" VARCHAR(255) NOT NULL,
-	"descripcion" VARCHAR(255) NOT NULL,
-	CONSTRAINT "estado_componente_pk" PRIMARY KEY ("id_estado")
-)
+CREATE TABLE "componentes_en_maquina" (
+  "id_componente_en_maquina" serial PRIMARY KEY NOT NULL,
+  "id_maquina" integer NOT NULL,
+  "id_estado" integer NOT NULL
+);
 
-CREATE TABLE "public.mantencion" (
-	"id_mantencion" serial NOT NULL,
-	"id_maquina" BINARY NOT NULL,
-	"descripcion" VARCHAR(255) NOT NULL,
-	"fecha_inicio" DATETIME NOT NULL,
-	"fecha_termino" DATETIME NOT NULL,
-	CONSTRAINT "mantencion_pk" PRIMARY KEY ("id_mantencion")
-)
+CREATE TABLE "estado_componente" (
+  "id_estado" serial UNIQUE PRIMARY KEY NOT NULL,
+  "estado" VARCHAR(255) NOT NULL,
+  "descripcion" VARCHAR(255) NOT NULL
+);
 
-CREATE TABLE "public.mantencion_componente" (
-	"id_reparacion_componente" serial NOT NULL,
-	"id_componente" integer NOT NULL,
-	"id_mantencion" integer NOT NULL,
-	CONSTRAINT "mantencion_componente_pk" PRIMARY KEY ("id_reparacion_componente")
-)
+CREATE TABLE "mantencion" (
+  "id_mantencion" serial PRIMARY KEY NOT NULL,
+  "id_maquina" integer NOT NULL,
+  "descripcion" VARCHAR(255) NOT NULL,
+  "fecha_inicio" timestamp NOT NULL,
+  "fecha_termino" timestamp NOT NULL
+);
 
-CREATE TABLE "public.mantencion_preventiva" (
-	"id_mantencion_preventiva" serial NOT NULL,
-	"descripcion" VARCHAR(255) NOT NULL,
-	"id_componente" integer NOT NULL,
-	"id_estado_componente" integer NOT NULL,
-	CONSTRAINT "mantencion_preventiva_pk" PRIMARY KEY ("id_mantencion_preventiva")
-)
+CREATE TABLE "mantencion_componente" (
+  "id_reparacion_componente" serial PRIMARY KEY NOT NULL,
+  "id_componente" integer NOT NULL,
+  "id_mantencion" integer NOT NULL
+);
 
-ALTER TABLE "maquina" ADD CONSTRAINT "maquina_fk0" FOREIGN KEY ("id_estado") REFERENCES "estado_maquina"("id_estado");
+CREATE TABLE "mantencion_preventiva" (
+  "id_mantencion_preventiva" serial PRIMARY KEY NOT NULL,
+  "descripcion" VARCHAR(255) NOT NULL,
+  "id_componente" integer NOT NULL,
+  "id_estado_componente" integer NOT NULL
+);
 
+ALTER TABLE "maquina" ADD FOREIGN KEY ("id_estado") REFERENCES "estado_maquina" ("id_estado");
 
-ALTER TABLE "incidentes" ADD CONSTRAINT "incidentes_fk0" FOREIGN KEY ("id_maquina") REFERENCES "maquina"("id_maquina");
+ALTER TABLE "incidentes" ADD FOREIGN KEY ("id_maquina") REFERENCES "maquina" ("id_maquina");
 
-ALTER TABLE "reparacion" ADD CONSTRAINT "reparacion_fk0" FOREIGN KEY ("id_maquina") REFERENCES "maquina"("id_maquina");
-ALTER TABLE "reparacion" ADD CONSTRAINT "reparacion_fk1" FOREIGN KEY ("id_componentes") REFERENCES "reparacion_componente"("id_reparacion_componente");
+ALTER TABLE "reparacion" ADD FOREIGN KEY ("id_maquina") REFERENCES "maquina" ("id_maquina");
 
-ALTER TABLE "reparacion_componente" ADD CONSTRAINT "reparacion_componente_fk0" FOREIGN KEY ("id_componente") REFERENCES "componente"("id_componente");
-ALTER TABLE "reparacion_componente" ADD CONSTRAINT "reparacion_componente_fk1" FOREIGN KEY ("id_mantencion") REFERENCES "reparacion"("id_reparacion");
+ALTER TABLE "reparacion" ADD FOREIGN KEY ("id_componentes") REFERENCES "reparacion_componente" ("id_reparacion_componente");
 
-ALTER TABLE "componente" ADD CONSTRAINT "componente_fk0" FOREIGN KEY ("componente_padre") REFERENCES "componente"("id_componente");
+ALTER TABLE "reparacion_componente" ADD FOREIGN KEY ("id_componente") REFERENCES "componente" ("id_componente");
 
-ALTER TABLE "componentes_en_maquina" ADD CONSTRAINT "componentes_en_maquina_fk0" FOREIGN KEY ("id_maquina") REFERENCES "maquina"("id_maquina");
-ALTER TABLE "componentes_en_maquina" ADD CONSTRAINT "componentes_en_maquina_fk1" FOREIGN KEY ("id_estado") REFERENCES "estado_componente"("id_estado");
+ALTER TABLE "reparacion_componente" ADD FOREIGN KEY ("id_mantencion") REFERENCES "reparacion" ("id_reparacion");
 
+ALTER TABLE "componente" ADD FOREIGN KEY ("componente_padre") REFERENCES "componente" ("id_componente");
 
-ALTER TABLE "mantencion" ADD CONSTRAINT "mantencion_fk0" FOREIGN KEY ("id_maquina") REFERENCES "maquina"("id_maquina");
+ALTER TABLE "componentes_en_maquina" ADD FOREIGN KEY ("id_maquina") REFERENCES "maquina" ("id_maquina");
 
-ALTER TABLE "mantencion_componente" ADD CONSTRAINT "mantencion_componente_fk0" FOREIGN KEY ("id_componente") REFERENCES "componente"("id_componente");
-ALTER TABLE "mantencion_componente" ADD CONSTRAINT "mantencion_componente_fk1" FOREIGN KEY ("id_mantencion") REFERENCES "mantencion"("id_mantencion");
+ALTER TABLE "componentes_en_maquina" ADD FOREIGN KEY ("id_estado") REFERENCES "estado_componente" ("id_estado");
 
-ALTER TABLE "mantencion_preventiva" ADD CONSTRAINT "mantencion_preventiva_fk0" FOREIGN KEY ("id_componente") REFERENCES "componente"("id_componente");
-ALTER TABLE "mantencion_preventiva" ADD CONSTRAINT "mantencion_preventiva_fk1" FOREIGN KEY ("id_estado_componente") REFERENCES "estado_componente"("id_estado");
+ALTER TABLE "mantencion" ADD FOREIGN KEY ("id_maquina") REFERENCES "maquina" ("id_maquina");
 
+ALTER TABLE "mantencion_componente" ADD FOREIGN KEY ("id_componente") REFERENCES "componente" ("id_componente");
 
+ALTER TABLE "mantencion_componente" ADD FOREIGN KEY ("id_mantencion") REFERENCES "mantencion" ("id_mantencion");
 
+ALTER TABLE "mantencion_preventiva" ADD FOREIGN KEY ("id_componente") REFERENCES "componente" ("id_componente");
 
-
-
-
-
-
-
-
-
+ALTER TABLE "mantencion_preventiva" ADD FOREIGN KEY ("id_estado_componente") REFERENCES "estado_componente" ("id_estado");
